161559ef6a5dfcc6154e38dee60cc9ea3f5cbe57
yl <1062968012@qq.com>
2016-08-31 03:56:06 -0400

    change

--- FBC/alloc.sh
+++ FBC/alloc.sh
@@ -1,5 +1,5 @@
 IFS=$(echo -en "\n\b") #为了处理文件名中的空格、TAB
-cd /yl/try #把try中文件拷到copy中
+cd /yl/test/try #把try中文件拷到copy中
 j=0
 # for i in `ls -1`
 # do
@@ -11,7 +11,7 @@ j=0
 
 for dir in `find . -type d`
 do 
-	dir_r="/yl/copy"`echo $dir|sed 's/.//'`
+	dir_r="/yl/test/copy"`echo $dir|sed 's/.//'`
 	mkdir $dir_r
 done
  #try git
@@ -19,8 +19,8 @@ done
 	
 for file in `find . -type f`
 do  
-	echo "/yl/copy"`echo $file|sed 's/.//'` >>/home/yl/file.txt
-	touch "/yl/copy"`echo $file|sed 's/.//'`
+	echo "/yl/test/copy"`echo $file|sed 's/.//'` >>/home/yl/file.txt
+	touch "/yl/test/copy"`echo $file|sed 's/.//'`
 	folder_list[j]="imap /copy"`echo $file|sed 's/.//'`
 	j=`expr $j + 1`
 done
@@ -33,10 +33,10 @@ cd /home/yl
   # echo ${folder_list[i]}
 # done
 
-dumpe2fs /dev/sdc1 | grep bitmap | awk '{print $4}' | awk -F ',' '{print $1}' > bitmap.txt
+dumpe2fs /dev/sdb3 | grep bitmap | awk '{print $4}' | awk -F ',' '{print $1}' > bitmap.txt
 	
 for ((i=0;i<j;i++))
 do
-	debugfs -R ${folder_list[i]} /dev/sdb1| grep located | awk '{print $4"\t"$6}' |awk -F ',' '{print $1"\t"$2}'>> test.txt
+	debugfs -R ${folder_list[i]} /dev/sdb3| grep located | awk '{print $4"\t"$6}' |awk -F ',' '{print $1"\t"$2}'>> test.txt
 done
 
